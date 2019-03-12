904b55be54faaa04cecc2380d04fc252327b4348
Eli Bendersky <eliben@google.com>
2019-03-12 09:21:51 -0700

    Add error

--- runtimevar-examples/rvsample.go
+++ runtimevar-examples/rvsample.go
@@ -8,11 +8,11 @@ import (
 	"gocloud.dev/runtimevar"
 	_ "gocloud.dev/runtimevar/runtimeconfigurator"
 	runtimeconfig "google.golang.org/genproto/googleapis/cloud/runtimeconfig/v1beta1"
+	"google.golang.org/grpc/status"
 )
 
-func main() {
+func snpsht() {
 	ctx := context.Background()
-
 	v, err := runtimevar.OpenVariable(ctx, "runtimeconfigurator://eliben-test1/eliben-testconfig/key1?decoder=string")
 	if err != nil {
 		log.Fatal(err)
@@ -29,4 +29,25 @@ func main() {
 	if s.As(&rcv) {
 		fmt.Println(rcv.UpdateTime)
 	}
+}
+
+func erroras() {
+	ctx := context.Background()
+	v, err := runtimevar.OpenVariable(ctx, "runtimeconfigurator://eliben-test1/eliben-teconfig/key1?decoder=string")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = v.Watch(ctx)
+	if err != nil {
+		var s *status.Status
+		if v.ErrorAs(err, &s) {
+			fmt.Println(s.Code())
+		}
+	}
+}
+
+func main() {
+	snpsht()
+	erroras()
 }
