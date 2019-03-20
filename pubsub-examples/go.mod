e4e1b7599b3f369903d5b976b9da5df1fa171572
Eli Bendersky <eliben@google.com>
2019-03-20 14:43:19 -0700

    More examples

--- pubsub-examples/go.mod
+++ pubsub-examples/go.mod
@@ -2,6 +2,11 @@ module pbsample
 
 go 1.12
 
-require gocloud.dev v0.12.0
+require (
+	cloud.google.com/go v0.37.0
+	gocloud.dev v0.12.0
+	google.golang.org/genproto v0.0.0-20190306203927-b5d61aea6440
+	google.golang.org/grpc v1.19.0
+)
 
 replace gocloud.dev => ../../go-cloud
--- pubsub-examples/pbsample.go
+++ pubsub-examples/pbsample.go
@@ -7,11 +7,13 @@ import (
 
 	"gocloud.dev/pubsub"
 	_ "gocloud.dev/pubsub/gcppubsub"
+
+	pbraw "cloud.google.com/go/pubsub/apiv1"
+	pbapi "google.golang.org/genproto/googleapis/pubsub/v1"
+	"google.golang.org/grpc/status"
 )
 
-// To publish a message here, post a message to the subscription via the GCP
-// console
-func main() {
+func rcv() {
 	ctx := context.Background()
 	sub, err := pubsub.OpenSubscription(ctx, "gcppubsub://eliben-test1/test1")
 	if err != nil {
@@ -24,4 +26,62 @@ func main() {
 	}
 	fmt.Println("got Message", msg)
 	msg.Ack()
+}
+
+func msgas() {
+	ctx := context.Background()
+	sub, err := pubsub.OpenSubscription(ctx, "gcppubsub://eliben-test1/test1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msg, err := sub.Receive(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var pm *pbapi.PubsubMessage
+	if msg.As(&pm) {
+		fmt.Println(pm.GetAttributes())
+	}
+	fmt.Println("got Message", msg)
+	msg.Ack()
+}
+
+func subas() {
+	ctx := context.Background()
+	sub, err := pubsub.OpenSubscription(ctx, "gcppubsub://eliben-test1/test1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var sc *pbraw.SubscriberClient
+	if sub.As(&sc) {
+		fmt.Println(sc.CallOptions)
+	}
+}
+
+func suberroras() {
+	ctx := context.Background()
+	sub, err := pubsub.OpenSubscription(ctx, "gcppubsub://eliben-test1/test100")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msg, err := sub.Receive(ctx)
+	if err != nil {
+		var s *status.Status
+		if sub.ErrorAs(err, &s) {
+			fmt.Println(s.Code())
+		}
+		log.Fatal(err)
+	}
+	msg.Ack()
+}
+
+// To publish a message here, post a message to the subscription via the GCP
+// console
+func main() {
+	//rcv()
+	//msgas()
+	//subas()
+	suberroras()
 }
