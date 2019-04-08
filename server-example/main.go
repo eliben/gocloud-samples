d6a2b8e30b509dbffcc2e7e79f4272ec5513fbf5
Eli Bendersky <eliben@google.com>
2019-04-08 14:33:37 -0700

    Adding stackdriver, not working yet

--- server-example/main.go
+++ server-example/main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 
+	"contrib.go.opencensus.io/exporter/stackdriver/monitoredresource"
+	"go.opencensus.io/trace"
+	"gocloud.dev/gcp"
 	"gocloud.dev/server"
+	"gocloud.dev/server/sdserver"
 )
 
 func main() {
+	ctx := context.Background()
+	credentials, err := gcp.DefaultCredentials(ctx)
+	fmt.Println(credentials)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	tokenSource := gcp.CredentialsTokenSource(credentials)
+	fmt.Println(tokenSource)
+
+	projectID, err := gcp.DefaultProjectID(credentials)
+	fmt.Println(projectID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mr := monitoredresource.Autodetect()
+	fmt.Println("mr", mr)
+	exporter, _, err := sdserver.NewExporter(projectID, tokenSource, mr)
+	fmt.Println("exporter", exporter)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "Hello\n")
@@ -16,6 +43,15 @@ func main() {
 		fmt.Fprintf(w, "Welcome to the home page!\n")
 	})
 
-	s := server.New(nil)
+	options := &server.Options{
+		RequestLogger:         sdserver.NewRequestLogger(),
+		HealthChecks:          nil,
+		TraceExporter:         nil,
+		DefaultSamplingPolicy: trace.AlwaysSample(),
+		Driver:                &server.DefaultDriver{},
+	}
+
+	s := server.New(options)
+	fmt.Println("Server", s)
 	s.ListenAndServe("localhost:8080", mux)
 }
