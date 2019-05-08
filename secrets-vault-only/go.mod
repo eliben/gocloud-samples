76ef2af1feb9590c4e9f9ac5e378221bfe020afa
Eli Bendersky <eliben@google.com>
2019-05-08 13:23:13 -0700

    Example using vault with the latest tip of go-cloud

---
+++ secrets-vault-only/go.mod
@@ -0,0 +1,7 @@
+module secretsample
+
+go 1.12
+
+require gocloud.dev v0.13.0
+
+replace gocloud.dev => ../../go-cloud
---
+++ secrets-vault-only/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/vault/api"
+	"gocloud.dev/secrets/vault"
+)
+
+// To run this, first start a Vault server running.
+//
+// An easy way is to run ./secrets/vault/localvault.sh from the root dir of
+// the go-cloud repository (it sets VAULT_DEV_ROOT_TOKEN_ID to "faketoken").
+func main() {
+	ctx := context.Background()
+	client, err := vault.Dial(ctx, &vault.Config{
+		Token: "faketoken",
+		APIConfig: api.Config{
+			Address: "http://127.0.0.1:8200",
+		},
+	})
+	if _, err := client.Logical().Write("sys/mounts/transit", map[string]interface{}{"type": "transit"}); err != nil {
+		log.Fatal(err)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Construct a *secrets.Keeper.
+	keeper := vault.OpenKeeper(client, "my-key", nil)
+	defer keeper.Close()
+
+	// Now we can use keeper to encrypt or decrypt.
+	plaintext := []byte("Hello, Secrets!")
+	ciphertext, err := keeper.Encrypt(ctx, plaintext)
+	if err != nil {
+		log.Fatal(err)
+	}
+	decrypted, err := keeper.Decrypt(ctx, ciphertext)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(decrypted)
+}
