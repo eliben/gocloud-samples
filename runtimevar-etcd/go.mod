023d1548e55eed73c876d749ed89db1a11fdb930
Eli Bendersky <eliben@google.com>
2019-05-28 14:15:25 -0700

    Real etcdvar example

--- runtimevar-etcd/go.mod
+++ runtimevar-etcd/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/coreos/etcd v3.3.12+incompatible // indirect
 	github.com/gogo/protobuf v1.2.1 // indirect
 	go.etcd.io/etcd v3.3.12+incompatible
+	gocloud.dev v0.13.0
 	google.golang.org/grpc v1.20.1 // indirect
 )
--- runtimevar-etcd/sample.go
+++ runtimevar-etcd/sample.go
@@ -1,40 +1,48 @@
-package sample
+package main
 
 import (
 	"context"
 	"fmt"
-	"math/rand"
+	"log"
 	"time"
 
 	"go.etcd.io/etcd/clientv3"
-	"go.etcd.io/etcd/clientv3/concurrency"
+	"gocloud.dev/runtimevar"
+	"gocloud.dev/runtimevar/etcdvar"
 )
 
-func KeepAlive(ctx context.Context, cli *clientv3.Client, srv, addr string, ttl int) (err error) {
-	for {
-		err = keepAlive(ctx, cli, srv, addr, ttl)
-		if err == context.DeadlineExceeded || err == context.Canceled {
-			return
-		}
-		time.Sleep(time.Duration(rand.Int63n(10)) * time.Millisecond)
-	}
+// MyConfig is a sample configuration struct.
+type MyConfig struct {
+	Server string
+	Port   int
 }
 
-func keepAlive(ctx context.Context, cli *clientv3.Client, srv, addr string, ttl int) error {
-	sess, err := concurrency.NewSession(cli, concurrency.WithContext(ctx), concurrency.WithTTL(ttl))
+func main() {
+	// Connect to the etcd server.
+	client, err := clientv3.NewFromURL("localhost:2379")
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	defer sess.Close()
 
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-sess.Done():
-		return fmt.Errorf("%s/%s exit", srv, addr)
+	// Create a decoder for decoding JSON strings into MyConfig.
+	decoder := runtimevar.NewDecoder(MyConfig{}, runtimevar.JSONDecode)
+
+	// Construct a *runtimevar.Variable that watches the variable.
+	// The etcd variable being referenced should have a JSON string that
+	// decodes into MyConfig.
+	v, err := etcdvar.OpenVariable(client, "cfg-variable-name", decoder, nil)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer v.Close()
+
+	// We can now read the current value of the variable from v.
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 150*time.Millisecond)
+	defer cancelFunc()
+	snapshot, err := v.Latest(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg := snapshot.Value.(MyConfig)
+	fmt.Println(cfg)
 }
-
-//func TestGocloud() *runtimevar.Variable {
-//return nil
-//}
