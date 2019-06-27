fbac5f4063c787562f742b18c3634bbc8dd9a261
Eli Bendersky <eliben@google.com>
2019-06-27 15:43:34 -0700

    Added get/set and error check

--- docstore-sample/mongo-example.go
+++ docstore-sample/mongo-example.go
@@ -4,10 +4,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"gocloud.dev/docstore"
 	_ "gocloud.dev/docstore/mongodocstore"
+	"gocloud.dev/gcerrors"
 )
 
 type Player struct {
@@ -17,13 +19,28 @@ type Player struct {
 }
 
 func main() {
-
 	ctx := context.Background()
 
-	// docstore.OpenCollection creates a *docstore.Collection from a URL.
-	coll, err := docstore.OpenCollection(ctx, "mongo://my-db/my-collection")
+	coll, err := docstore.OpenCollection(ctx, "mongo://my-db/my-collection?id_field=Name")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer coll.Close()
+
+	name := "Patr"
+	fmt.Println("Fetching player", name)
+	p := &Player{Name: name}
+	err = coll.Get(ctx, p)
+	if err != nil {
+		// if not found, insert
+		if gcerrors.Code(err) == gcerrors.NotFound {
+			fmt.Println("Inserting")
+			err = coll.Create(ctx, &Player{Name: name, Score: 10})
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		log.Fatal(err)
+	}
+	fmt.Println(p)
 }
